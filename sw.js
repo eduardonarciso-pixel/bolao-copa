73f0eb9126a31c15680f2bd1b8200cdc4f759740
eduardonarciso-pixel <eduardo.narciso@gmail.com>
2026-06-07 07:31:18 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'bolao-copa-v1';
+const ARQUIVOS = [
+  '/bolao-copa/',
+  '/bolao-copa/index.html',
+  '/bolao-copa/style.css',
+  '/bolao-copa/app.js',
+  '/bolao-copa/firebase-config.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ARQUIVOS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
